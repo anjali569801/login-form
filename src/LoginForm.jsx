507d13950365966500f33f5aa13fc52e4a273715
Anjali <103408967+anjali569801@users.noreply.github.com>
2023-02-27 07:15:47 +0000

    pass the dummy function in button on click

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -7,27 +7,46 @@ function LoginForm() {
 
   const[email,SetEmail]=useState('')
 
-  const[password,SetPAssword]=useState('')
+  const[password,SetPassword]=useState('')
 
   
   const handleChange=(event)=>{
     if(event.target.name=="email"){
       SetEmail(event.target.value)
     }else if(event.target.name=="password"){
-      SetPAssword(event.target.value)
+      SetPassword(event.target.value)
     }
-    
   }
- 
+  
+  //         OR
+  
+  // const onEmailChange=(event)=>{
+  //   SetEmail(event.target.value);
+  // }
+  // const onPasswordChange=(event)=>{
+  //   SetPAssword(event.target.value);
+  // }
+
+ const onButtonClick=()=>{
+   console.log("email is ",email, "password is ",password);
+   SetEmail('')
+    SetPassword('')
+ }
+
+  const set=()=>{
+    SetEmail('')
+    SetPassword('')
+  }
+  
   return(
     <div className=" space-y-8 w-64 h-64">
       <div className="flex flex-col space-y-2">
       <Input placeholder="email" value={email} name="email" onChange={handleChange}></Input>
-      <Input name="myPassowrd" placeholder="password" value={password} onChange={handleChange}></Input>
+      <Input name="password" placeholder="password" value={password} onChange={handleChange}></Input>
         </div>
       <div className="space-y-2 mt-2 ">
-     <Button>login</Button>
-      <Button>reset</Button>
+     <Button onClick={onButtonClick}>login</Button>
+      <Button onClick={set}>reset</Button>
          <Link className="text-white ml-16">forget password?</Link>
         </div>
      
