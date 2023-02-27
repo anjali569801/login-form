87ac30430b9703a0a6b522b40e9a69c357ac1aa4
Anjali <103408967+anjali569801@users.noreply.github.com>
2023-02-27 07:03:27 +0000

    get the value of input from single handle change

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -9,18 +9,21 @@ function LoginForm() {
 
   const[password,SetPAssword]=useState('')
 
-  const onEmailChange=(event)=>{
-    SetEmail(event.target.value);
-  }
-  const onPasswordChange=(event)=>{
-    SetPAssword(event.target.value);
+  
+  const handleChange=(event)=>{
+    if(event.target.name=="email"){
+      SetEmail(event.target.value)
+    }else if(event.target.name=="password"){
+      SetPAssword(event.target.value)
+    }
+    
   }
  
   return(
     <div className=" space-y-8 w-64 h-64">
       <div className="flex flex-col space-y-2">
-      <Input placeholder="email" value={email} name="email" onChange={onEmailChange}></Input>
-      <Input name="myPassowrd" placeholder="password" value={password} onChange={onPasswordChange}></Input>
+      <Input placeholder="email" value={email} name="email" onChange={handleChange}></Input>
+      <Input name="myPassowrd" placeholder="password" value={password} onChange={handleChange}></Input>
         </div>
       <div className="space-y-2 mt-2 ">
      <Button>login</Button>
