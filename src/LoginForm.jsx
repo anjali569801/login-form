5239128ccb576a51cdb860dcde800f820ec3ebf9
Anjali <103408967+anjali569801@users.noreply.github.com>
2023-02-27 07:00:46 +0000

    get the value of input tag with the help of separate hadle change

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Input from './Input';
 import {Link} from 'react-router-dom';
 
 function LoginForm() {
 
+  const[email,SetEmail]=useState('')
+
+  const[password,SetPAssword]=useState('')
+
+  const onEmailChange=(event)=>{
+    SetEmail(event.target.value);
+  }
+  const onPasswordChange=(event)=>{
+    SetPAssword(event.target.value);
+  }
  
   return(
     <div className=" space-y-8 w-64 h-64">
       <div className="flex flex-col space-y-2">
-      <Input placeholder="email" value={""} onChange={event}></Input>
-      <Input placeholder="email" value={""} onChange={event}></Input>
+      <Input placeholder="email" value={email} name="email" onChange={onEmailChange}></Input>
+      <Input name="myPassowrd" placeholder="password" value={password} onChange={onPasswordChange}></Input>
         </div>
       <div className="space-y-2 mt-2 ">
      <Button>login</Button>
