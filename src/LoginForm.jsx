7cc90268991b5a3f1f49e16e5c9963d195e00144
Anjali <103408967+anjali569801@users.noreply.github.com>
2023-02-27 10:06:19 +0000

    use formik

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,56 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useFormik } from 'formik'
 import Button from './Button';
 import Input from './Input';
 import {Link} from 'react-router-dom';
 
 function LoginForm() {
 
-  const[email,SetEmail]=useState('')
 
-  const[password,SetPassword]=useState('')
-
+  const onFormSubmit=(values)=>{
+   console.log("email",values.email,"password is",values.password);
   
-  const handleChange=(event)=>{
-    if(event.target.name=="email"){
-      SetEmail(event.target.value)
-    }else if(event.target.name=="password"){
-      SetPassword(event.target.value)
-    }
-  }
-  
-  //         OR
-  
-  // const onEmailChange=(event)=>{
-  //   SetEmail(event.target.value);
-  // }
-  // const onPasswordChange=(event)=>{
-  //   SetPAssword(event.target.value);
-  // }
-
- const onButtonClick=()=>{
-   console.log("email is ",email, "password is ",password);
-   SetEmail('')
-    SetPassword('')
  }
 
-  const set=()=>{
-    SetEmail('')
-    SetPassword('')
-  }
+  const {handleChange,handleBlur,values,handleSubmit,errors,resetForm} = useFormik({
+    initialValues:{
+      email:"",
+      password:"",
+    }
+  ,onSubmit:onFormSubmit,
+  })
   
   return(
-    <div className=" space-y-8 w-64 h-64">
+    <form onSubmit={handleSubmit} className=" space-y-8 w-64 h-64">
       <div className="flex flex-col space-y-2">
-      <Input placeholder="email" value={email} name="email" onChange={handleChange}></Input>
-      <Input name="password" placeholder="password" value={password} onChange={handleChange}></Input>
+      <Input onBlur={handleBlur}  placeholder="email" value={values.email} name="email" onChange={handleChange}/>
+      <Input onBlur={handleBlur}  name="password" placeholder="password" value={values.password} onChange={handleChange}/>
         </div>
       <div className="space-y-2 mt-2 ">
-     <Button onClick={onButtonClick}>login</Button>
-      <Button onClick={set}>reset</Button>
+     <Button>login</Button>
+      <Button onClick={resetForm}>reset</Button>
          <Link className="text-white ml-16">forget password?</Link>
         </div>
      
-    </div>
+    </form>
   );
 }
 
