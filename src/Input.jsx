4e7b38272d4fda8e33fff4ea8db39e7cb3768b35
Anjali <103408967+anjali569801@users.noreply.github.com>
2023-02-28 06:28:03 +0000

    install yup and  show errors with the help of validation schema

--- src/Input.jsx
+++ src/Input.jsx
@@ -5,7 +5,7 @@ function Input(props) {
  
   return(
     
-      <input id={props.id} name={props.name} required placeholder={props.placeholder} type={props.type}  value={props.value} onChange={props.onChange} className={'p-1 px-4 w-48 bg-transparent border border-white placeholder-white rounded-sm border-1' + (props.className)}></input>
+      <input id={props.id} name={props.name} required placeholder={props.placeholder} type={props.type}  value={props.value} onBlur={props.onBlur} onChange={props.onChange} className={'p-1 px-4 w-48 bg-transparent border border-white placeholder-white rounded-sm border-1' + (props.className)}></input>
    
   );
 }
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -3,32 +3,41 @@ import { useFormik } from 'formik'
 import Button from './Button';
 import Input from './Input';
 import {Link} from 'react-router-dom';
+import * as Yup from 'yup';
 
 function LoginForm() {
 
 
   const onFormSubmit=(values)=>{
    console.log("email",values.email,"password is",values.password);
-  
+    
  }
 
-  const {handleChange,handleBlur,values,handleSubmit,errors,resetForm} = useFormik({
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email(),
+    password: Yup.string().required().min(8).max(12)
+  })
+
+  const {handleChange,handleBlur,values,touched,handleSubmit,errors,resetForm} = useFormik({
     initialValues:{
       email:"",
       password:"",
     }
   ,onSubmit:onFormSubmit,
+    validationSchema,
   })
   
   return(
     <form onSubmit={handleSubmit} className=" space-y-8 w-64 h-64">
       <div className="flex flex-col space-y-2">
       <Input onBlur={handleBlur}  placeholder="email" value={values.email} name="email" onChange={handleChange}/>
+        {touched.email && errors.email && <span className="text-red-700">{errors.email}</span>}
       <Input onBlur={handleBlur}  name="password" placeholder="password" value={values.password} onChange={handleChange}/>
+        {touched.password && errors.password && <span className="text-red-700">{errors.password}</span>}
         </div>
       <div className="space-y-2 mt-2 ">
-     <Button>login</Button>
-      <Button onClick={resetForm}>reset</Button>
+     <Button type="submit">login</Button>
+      <Button type="button" onClick={resetForm}>reset</Button>
          <Link className="text-white ml-16">forget password?</Link>
         </div>
      
